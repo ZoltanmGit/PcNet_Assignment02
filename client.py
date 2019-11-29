022f92e8c52df78b6670bd6ff936764686d3a868
ZoltanmGit <58223994+ZoltanmGit@users.noreply.github.com>
2019-11-29 13:38:15 +0100

    Updated File

--- client.py
+++ client.py
@@ -2,29 +2,6 @@ import sys
 import json
 import time
 
-"""
-Készíts programot, ami leszimulálja az erőforrások lefoglalását és felszabadítását a JSON fájlban megadott topológia, kapacitások és igények alapján!
-
-Script paraméterezése: python3 client.py cs.json
-
-A program kimenete:
-
-    esemény sorszám. <esemény név>: <node1><-><node2> st:<szimuálciós idő> [- <sikeres/sikertelen>]
-
-Pl.:
-
-    igény foglalás: A<->C st:1 – sikeres
-
-    igény foglalás: B<->C st:2 – sikeres
-
-    igény felszabadítás: A<->C st:5
-
-    igény foglalás: D<->C st:6 – sikeres
-
-    igény foglalás: A<->C st:7 – sikertelen
-
-"""
-
 def GetLinkCapacityIndex(argPointA, argPointB): #Returns capacity of linked points
     for i in range(len(links)):
         for j in range(2):
